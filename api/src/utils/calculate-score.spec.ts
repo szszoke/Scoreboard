6745e361fb930385ef4d20862e363819aa18c606
Szabolcs Szőke <szszoke.code@gmail.com>
2018-08-14 13:49:20 +0200

    Move test mock data to a separate file

--- api/src/utils/calculate-score.spec.ts
+++ api/src/utils/calculate-score.spec.ts
@@ -1,73 +1,48 @@
 import { calculateScore } from './calculate-score';
 import { expect } from 'chai';
 import 'mocha';
+import {
+  ONE_STRIKE,
+  TWO_STRIKES,
+  THREE_STRIKES,
+  FOUR_STRIKES,
+  ONE_SPARE,
+  TWO_SPARES,
+  THREE_SPARES,
+  FOUR_SPARES,
+  STRIKE_AND_SPARE,
+  STRIKE_AND_OPEN,
+  SPARE_AND_STRIKE,
+  TWO_SPARES_IN_ROW,
+  SPARE_AND_OPEN,
+  FULL_GAME_ALL_SPARES,
+  FULL_GAME_ALL_STRIKES,
+  FULL_GAME_NINTH_SPARE,
+  FULL_GAME_NINTH_OPEN,
+} from '@/utils/mocks';
 
 describe('calculateScore', () => {
   describe('strikes in row', () => {
     it('one', () => {
-      const score = calculateScore([
-        {
-          first: 10,
-          second: 0,
-        },
-      ]);
+      const score = calculateScore(ONE_STRIKE);
 
       expect(score).to.equal(10);
     });
 
     it('two', () => {
-      const score = calculateScore([
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-      ]);
+      const score = calculateScore(TWO_STRIKES);
 
       expect(score).to.equal(30);
     });
 
     it('three', () => {
-      const score = calculateScore([
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-      ]);
+      const score = calculateScore(THREE_STRIKES);
 
       expect(score).to.equal(60);
     });
 
     it('four', () => {
-      const score = calculateScore([
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-      ]);
+      const score = calculateScore(FOUR_STRIKES);
 
       expect(score).to.equal(90);
     });
@@ -75,338 +50,81 @@ describe('calculateScore', () => {
 
   describe('spares in row', () => {
     it('one', () => {
-      const score = calculateScore([
-        {
-          first: 3,
-          second: 7,
-        },
-      ]);
+      const score = calculateScore(ONE_SPARE);
 
       expect(score).to.equal(10);
     });
 
     it('two', () => {
-      const score = calculateScore([
-        {
-          first: 1,
-          second: 9,
-        },
-        {
-          first: 2,
-          second: 8,
-        },
-      ]);
+      const score = calculateScore(TWO_SPARES);
 
       expect(score).to.equal(22);
     });
 
     it('three', () => {
-      const score = calculateScore([
-        {
-          first: 4,
-          second: 6,
-        },
-        {
-          first: 3,
-          second: 7,
-        },
-        {
-          first: 9,
-          second: 1,
-        },
-      ]);
+      const score = calculateScore(THREE_SPARES);
 
       expect(score).to.equal(42);
     });
 
     it('four', () => {
-      const score = calculateScore([
-        {
-          first: 2,
-          second: 8,
-        },
-        {
-          first: 6,
-          second: 4,
-        },
-        {
-          first: 0,
-          second: 10,
-        },
-        {
-          first: 3,
-          second: 7,
-        },
-      ]);
+      const score = calculateScore(FOUR_SPARES);
 
       expect(score).to.equal(49);
     });
   });
 
   it('a strike and a spare', () => {
-    const score = calculateScore([
-      {
-        first: 10,
-        second: 0,
-      },
-      {
-        first: 4,
-        second: 6,
-      },
-    ]);
+    const score = calculateScore(STRIKE_AND_SPARE);
 
     expect(score).to.equal(30);
   });
 
   it('a strike and an open', () => {
-    const score = calculateScore([
-      {
-        first: 10,
-        second: 0,
-      },
-      {
-        first: 3,
-        second: 1,
-      },
-    ]);
+    const score = calculateScore(STRIKE_AND_OPEN);
 
     expect(score).to.equal(18);
   });
 
   it('a spare and a strike', () => {
-    const score = calculateScore([
-      {
-        first: 7,
-        second: 3,
-      },
-      {
-        first: 10,
-        second: 0,
-      },
-    ]);
+    const score = calculateScore(SPARE_AND_STRIKE);
 
     expect(score).to.equal(30);
   });
 
   it('two spares in row', () => {
-    const score = calculateScore([
-      {
-        first: 7,
-        second: 3,
-      },
-      {
-        first: 1,
-        second: 9,
-      },
-    ]);
+    const score = calculateScore(TWO_SPARES_IN_ROW);
 
     expect(score).to.equal(21);
   });
 
   it('a spare and an open', () => {
-    const score = calculateScore([
-      {
-        first: 7,
-        second: 3,
-      },
-      {
-        first: 3,
-        second: 1,
-      },
-    ]);
+    const score = calculateScore(SPARE_AND_OPEN);
 
     expect(score).to.equal(17);
   });
 
   describe('full game', () => {
     it('all spares with bonus roll', () => {
-      const score = calculateScore([
-        {
-          first: 7,
-          second: 3,
-        },
-        {
-          first: 3,
-          second: 7,
-        },
-        {
-          first: 2,
-          second: 8,
-        },
-        {
-          first: 0,
-          second: 10,
-        },
-        {
-          first: 4,
-          second: 6,
-        },
-        {
-          first: 5,
-          second: 5,
-        },
-        {
-          first: 8,
-          second: 2,
-        },
-        {
-          first: 9,
-          second: 1,
-        },
-        {
-          first: 7,
-          second: 3,
-        },
-        {
-          first: 3,
-          second: 7,
-          third: 6,
-        },
-      ]);
+      const score = calculateScore(FULL_GAME_ALL_SPARES);
 
       expect(score).to.equal(147);
     });
 
     it('all strikes!', () => {
-      const score = calculateScore([
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 10,
-          third: 10,
-        },
-      ]);
+      const score = calculateScore(FULL_GAME_ALL_STRIKES);
 
       expect(score).to.equal(300);
     });
 
     it('the ninth is spare', () => {
-      const score = calculateScore([
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 7,
-          second: 3,
-        },
-        {
-          first: 10,
-          second: 10,
-          third: 10,
-        },
-      ]);
+      const score = calculateScore(FULL_GAME_NINTH_SPARE);
 
       expect(score).to.equal(277);
     });
 
     it('the ninth is open', () => {
-      const score = calculateScore([
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 10,
-          second: 0,
-        },
-        {
-          first: 6,
-          second: 3,
-        },
-        {
-          first: 10,
-          second: 10,
-          third: 10,
-        },
-      ]);
+      const score = calculateScore(FULL_GAME_NINTH_OPEN);
 
       expect(score).to.equal(264);
     });
---
+++ api/src/utils/mocks.ts
@@ -0,0 +1,339 @@
+import { IFrame } from '@common/frame';
+
+// Strikes
+export const ONE_STRIKE: IFrame[] = [
+  {
+    first: 10,
+    second: 0,
+  },
+];
+
+export const TWO_STRIKES: IFrame[] = [
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+];
+
+export const THREE_STRIKES: IFrame[] = [
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+];
+
+export const FOUR_STRIKES: IFrame[] = [
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+];
+
+// Spares
+export const ONE_SPARE: IFrame[] = [
+  {
+    first: 3,
+    second: 7,
+  },
+];
+
+export const TWO_SPARES: IFrame[] = [
+  {
+    first: 1,
+    second: 9,
+  },
+  {
+    first: 2,
+    second: 8,
+  },
+];
+
+export const THREE_SPARES: IFrame[] = [
+  {
+    first: 4,
+    second: 6,
+  },
+  {
+    first: 3,
+    second: 7,
+  },
+  {
+    first: 9,
+    second: 1,
+  },
+];
+
+export const FOUR_SPARES: IFrame[] = [
+  {
+    first: 2,
+    second: 8,
+  },
+  {
+    first: 6,
+    second: 4,
+  },
+  {
+    first: 0,
+    second: 10,
+  },
+  {
+    first: 3,
+    second: 7,
+  },
+];
+
+// Mixed
+export const STRIKE_AND_SPARE: IFrame[] = [
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 4,
+    second: 6,
+  },
+];
+
+export const STRIKE_AND_OPEN: IFrame[] = [
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 3,
+    second: 1,
+  },
+];
+
+export const SPARE_AND_STRIKE: IFrame[] = [
+  {
+    first: 7,
+    second: 3,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+];
+
+export const TWO_SPARES_IN_ROW: IFrame[] = [
+  {
+    first: 7,
+    second: 3,
+  },
+  {
+    first: 1,
+    second: 9,
+  },
+];
+
+export const SPARE_AND_OPEN: IFrame[] = [
+  {
+    first: 7,
+    second: 3,
+  },
+  {
+    first: 3,
+    second: 1,
+  },
+];
+
+export const FULL_GAME_ALL_SPARES: IFrame[] = [
+  {
+    first: 7,
+    second: 3,
+  },
+  {
+    first: 3,
+    second: 7,
+  },
+  {
+    first: 2,
+    second: 8,
+  },
+  {
+    first: 0,
+    second: 10,
+  },
+  {
+    first: 4,
+    second: 6,
+  },
+  {
+    first: 5,
+    second: 5,
+  },
+  {
+    first: 8,
+    second: 2,
+  },
+  {
+    first: 9,
+    second: 1,
+  },
+  {
+    first: 7,
+    second: 3,
+  },
+  {
+    first: 3,
+    second: 7,
+    third: 6,
+  },
+];
+
+export const FULL_GAME_ALL_STRIKES: IFrame[] = [
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 10,
+    third: 10,
+  },
+];
+
+export const FULL_GAME_NINTH_SPARE: IFrame[] = [
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 7,
+    second: 3,
+  },
+  {
+    first: 10,
+    second: 10,
+    third: 10,
+  },
+];
+
+export const FULL_GAME_NINTH_OPEN: IFrame[] = [
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 10,
+    second: 0,
+  },
+  {
+    first: 6,
+    second: 3,
+  },
+  {
+    first: 10,
+    second: 10,
+    third: 10,
+  },
+];
