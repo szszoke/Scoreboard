cca7d94759e01c09de93eb95146c9b106d399bf5
Szabolcs Szőke <szszoke.code@gmail.com>
2018-08-16 09:36:03 +0200

    Remove unused code

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 import { ScoreboardInputComponent } from './scoreboard-input/scoreboard-input.component';
 import { ScoreboardFramesComponent } from './scoreboard-frames/scoreboard-frames.component';
-import { ValidateScoresDirective } from './validate-scores.directive';
 import { ScoreboardFrameComponent } from './scoreboard-frame/scoreboard-frame.component';
 
 @NgModule({
@@ -16,10 +14,9 @@ import { ScoreboardFrameComponent } from './scoreboard-frame/scoreboard-frame.co
     ScoreboardComponent,
     ScoreboardInputComponent,
     ScoreboardFramesComponent,
-    ValidateScoresDirective,
     ScoreboardFrameComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
