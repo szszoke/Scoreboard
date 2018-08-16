937e147573bc1e02ec87034dcb4a01ed42ae2bb8
Szabolcs Szőke <szszoke.code@gmail.com>
2018-08-16 23:02:41 +0200

    Add end to end tests

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -1,14 +1,149 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
   it('should display title in header', () => {
-    page.navigateTo();
     expect(page.getHeaderText()).toEqual('Bowling scoreboard');
+  });
+
+  it('should display total score', () => {
+    expect(page.getTotalScore().isPresent()).toBeTruthy();
+  });
+
+  it('should display 10 scoreboard frames', () => {
+    expect(page.getScoreboardFrames().count()).toBe(10);
+  });
+
+  it('should display 11 buttons for inputting scores', () => {
+    expect(page.getScoreInputButtons().count()).toBe(11);
+  });
+
+  it('should display new game button', () => {
+    expect(page.getNewGameButton().isPresent()).toBeTruthy();
+  });
+
+  describe('initially', () => {
+    it('should not display game over message', () => {
+      expect(page.getGameOverMessage().isPresent()).toBeFalsy();
+    });
+  });
+
+  describe('first frame', () => {
+    it('should contain nothing', async () => {
+      const frameContent = await page.getFrameContent(0);
+
+      expect(frameContent.first).toBeUndefined();
+      expect(frameContent.second).toBeUndefined();
+      expect(frameContent.score).toBeUndefined();
+    });
+  });
+
+  describe('strike button clicked', () => {
+    it('should display score in first frame', async () => {
+      const button = page.getScoreInputButton(10);
+      button.click();
+      const frameContent = await page.getFrameContent(0);
+      expect(frameContent.first).toBe(10);
+      expect(frameContent.second).toBeFalsy();
+    });
+
+    it('should display total score in the bottom of first frame', async () => {
+      const button = page.getScoreInputButton(10);
+      button.click();
+      const frameContent = await page.getFrameContent(0);
+      expect(frameContent.score).toBe(10);
+    });
+
+    it('should update total score on the top of the page', async () => {
+      const button = page.getScoreInputButton(10);
+      button.click();
+      const frameContent = await page.getTotalScoreValue();
+      expect(frameContent).toBe(10);
+    });
+  });
+
+  describe('full game', () => {
+    it('should display game over message', async () => {
+      const button = page.getScoreInputButton(10);
+
+      for (let i = 0; i < 12; ++i) {
+        await button.click();
+      }
+
+      expect(page.getGameOverMessage().isPresent()).toBeTruthy();
+    });
+
+    it('clicking new game hides the game over message', async () => {
+      const button = page.getScoreInputButton(10);
+
+      for (let i = 0; i < 12; ++i) {
+        await button.click();
+      }
+
+      await page.getNewGameButton().click();
+
+      expect(page.getGameOverMessage().isPresent()).toBeFalsy();
+    });
+  });
+
+  describe('when rolling', () => {
+    it('should change the active frame', async () => {
+      const button = page.getScoreInputButton(10);
+      for (let i = 0; i < 10; ++i) {
+        const frameIndex = page.getFrameIndex(i);
+        const className = await frameIndex.getAttribute('class');
+        expect(className).toContain('active');
+        await button.click();
+      }
+    });
+
+    describe('strikes', () => {
+      it('should update the score based on the next frames', async () => {
+        const scores = [10, 20, 30];
+        const button = page.getScoreInputButton(10);
+        for (let i = 0; i < 3; ++i) {
+          await button.click();
+          const { score } = await page.getFrameContent(0);
+          expect(score).toBe(scores[i]);
+        }
+      });
+    });
+
+    describe('spares', () => {
+      it('should update the score based on the next frames', async () => {
+        const scores = [10, 13];
+        for (let i = 0; i < 2; ++i) {
+          await page.getScoreInputButton(3).click();
+          await page.getScoreInputButton(7).click();
+          const { score } = await page.getFrameContent(0);
+          expect(score).toBe(scores[i]);
+        }
+      });
+    });
+  });
+
+  describe('when rolling a spare', () => {
+    it('should disable some inputs after the first roll', async () => {
+      await page.getScoreInputButton(5).click();
+
+      for (let i = 0; i <= 5; ++i) {
+        const button = page.getScoreInputButton(i);
+        const enabled = await button.isEnabled();
+        expect(enabled).toBeTruthy();
+      }
+
+      for (let i = 6; i <= 10; ++i) {
+        const button = page.getScoreInputButton(i);
+        const enabled = await button.isEnabled();
+        expect(enabled).toBeFalsy();
+      }
+    });
   });
 });
--- client/e2e/src/app.po.ts
+++ client/e2e/src/app.po.ts
@@ -1,4 +1,5 @@
 import { browser, by, element } from 'protractor';
+import { IFrameWithScore } from '@app/scoreboard.service';
 
 export class AppPage {
   navigateTo() {
@@ -7,5 +8,66 @@ export class AppPage {
 
   getHeaderText() {
     return element(by.css('app-root header')).getText();
+  }
+
+  getScoreboardFrames() {
+    return element.all(by.tagName('app-scoreboard-frame'));
+  }
+
+  getScoreInputButtons() {
+    return element.all(by.css('app-scoreboard-input .input'));
+  }
+
+  getScoreInputButton(index: number) {
+    return this.getScoreInputButtons().get(index);
+  }
+
+  getNewGameButton() {
+    return element(by.cssContainingText('button', 'New game'));
+  }
+
+  getTotalScore() {
+    return element(by.css('app-scoreboard>h1'));
+  }
+
+  getGameOverMessage() {
+    return element(by.css('.game-over'));
+  }
+
+  async getTotalScoreValue(): Promise<number> {
+    const totalScoreText = await this.getTotalScore().getText();
+    const match = totalScoreText.match(/(Total score: )(\d+)/);
+
+    return Promise.resolve(+match[2]);
+  }
+
+  getFrameIndex(index: number) {
+    return this.getScoreboardFrames()
+      .get(index)
+      .element(by.css('.frame-index'));
+  }
+
+  async getFrameContent(index: number): Promise<IFrameWithScore> {
+    const frame = this.getScoreboardFrames().get(index);
+
+    const firstRollText = await frame.element(by.css('.first-roll')).getText();
+    const secondRollText = await frame
+      .element(by.css('.second-roll'))
+      .getText();
+    const thirdRoll = frame.element(by.css('.third-roll'));
+    const thirdRollText = (await thirdRoll.isPresent())
+      ? await thirdRoll.getText()
+      : undefined;
+    const totalScoreText = await frame
+      .element(by.css('.total-score'))
+      .getText();
+
+    return Promise.resolve({
+      first: firstRollText ? +firstRollText : undefined,
+      second: secondRollText ? +secondRollText : undefined,
+      third: thirdRollText ? +thirdRollText : undefined,
+      score: totalScoreText ? +totalScoreText : undefined,
+      index,
+    });
   }
 }
